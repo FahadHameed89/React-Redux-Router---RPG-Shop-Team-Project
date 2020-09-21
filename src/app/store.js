a3e8ce8ab69175b0b44324cddd3cd73dbffbe42f
Aaron Barthel <aaron.barthel@live.ca>
2020-09-21 14:50:55 -0600

    Setup preloaded state for products

---
+++ src/app/store.js
@@ -0,0 +1,49 @@
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+export default () => {
+  const fetchEquipment = fetch('data/equipment.json')
+                          .then(res => res.text())
+                          .then(res => JSON.parse(res));
+
+  const fetchUtilities = fetch('data/utilities.json')
+                          .then(res => res.text())
+                          .then(res => JSON.parse(res));
+
+  const fetchSignin = fetch('data/signin.json')
+                        .then(res => res.text())
+                        .then(res => JSON.parse(res));
+
+
+  const mapProducts = (products, dataset) => {
+    return dataset.reduce((state, current) => { state[current.name] = current; return state; }, products);
+  }
+
+  const mapSignin = (signin, dataset) => {
+    return dataset.reduce((state, current) => {state[current.clan] = current; return state; }, signin);
+  }
+
+  return Promise
+    .all([fetchEquipment, fetchUtilities, fetchSignin])
+    .then(rawData => {
+      let products = {};
+      let signIn = {};
+
+      products = mapProducts(products, rawData[0]);
+      products = mapProducts(products, rawData[1]);
+      // signIn = mapSignin(signIn, rawData[2]);
+
+      return {
+        // clans: signIn,
+        products: products
+      }
+    })
+    .then(preloadedState => {
+      return createStore(
+        rootReducer,
+        preloadedState,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    })
+
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStoreAsync from './app/store';
+
 import App from './app/App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+configureStoreAsync()
+  .then(res => {
+    const store = res;
+
+    ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  })
