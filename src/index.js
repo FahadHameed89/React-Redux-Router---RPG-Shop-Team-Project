0fdbb01c1fdf2baa67760425f9cfeaf38f9e1d09
Irwin-S <irwin.r.singh@gmail.com>
2020-09-21 17:55:38 -0600

    Developed statistics JSK and CSS

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import Statistics from './components/stats';
+import './components/stats.css';
+
 ReactDOM.render(
   <React.StrictMode>
+    <Statistics />
   </React.StrictMode>,
   document.getElementById('root')
 );
