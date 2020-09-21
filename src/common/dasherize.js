7a79b1230b0f7444ff2f08517e9e88820d940862
Aaron Barthel <aaron.barthel@live.ca>
2020-09-21 16:40:18 -0600

    Add citation for dasherize

--- src/common/dasherize.js
+++ src/common/dasherize.js
@@ -1,3 +1,10 @@
+// Citation: (dasherize)
+// https://coderwall.com/p/hpq7sq/undescorize-dasherize-capitalize-string-prototype
+//
+// Looked up a clean example of handling the first character as an uppercase character
+// problem. Wasn't exactly sure how to handle that cleanly.
+// -Aaron
+
 /**
  * Returns new string with all whitespace removed and all capital
  * letters replaced with a dash followed by the lower case version of
