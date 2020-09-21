abd49776f21d87750baa6e067f963d02b29b85ce
Aaron Barthel <aaron.barthel@live.ca>
2020-09-21 15:30:50 -0600

    Add Citation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import configureStoreAsync from './app/store';
 import App from './app/App';
 import './index.css';
 
+// Citation:
+// https://stackoverflow.com/questions/37393176/redux-loading-initial-state-asynchronously
+//
+// The example given in the above link gave me the initial structure and idea for loading
+// our json data asyncronously which forced us to delay the creation of our store objects
+// which forced the delay of creating our react render hook.
+//
 configureStoreAsync()
   .then(res => {
     const store = res;
