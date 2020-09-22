591c0bf7cf9ad96216147d3bf4282727237d28ac
Aaron Barthel <aaron.barthel@live.ca>
2020-09-22 14:35:46 -0600

    Add ProductImage to Products Page

--- src/product/Product.jsx
+++ src/product/Product.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import ProductImage from './ProductImage';
 
 export default () => {
   const { id } = useParams();
+  const product = useSelector(state => state.products[id])
 
   return (
-    <p>Hello from product {id}</p>
+    <main>
+      <ProductImage rarity={product.rarity} path={product.image} />
+      <p>Add To Cart Component</p>
+      <header>
+        <h1></h1>
+      </header>
+      <p>Description</p>
+      <p>Stats</p>
+    </main>
   );
 }
