75b9f6a8a763314b3b9beb5959b76190194e5cac
Aaron Barthel <aaron.barthel@live.ca>
2020-09-22 20:52:57 -0600

    Clean-up description component

--- src/product/ProductDescription.jsx
+++ src/product/ProductDescription.jsx
@@ -1,42 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-// import getAllProducts from './productReducer';
 
+export default (props) => {
+    const className = props.className || '';
 
+    // Check if props.decription is an array, if it is
+    // then use it as the description otherwise assume
+    // props.description is just a string so wrap it in an
+    // array so that we only have to handle one type of
+    // data in our render phase.
+    const description = Array.isArray(props.description)
+                            ? props.description : [props.description];
 
-/**
- * What needs to happen
- * ====================
- *
- * - Grab data item description from json file.
- * - Output description into JSX.
- * Note: When outputting description. Need to check if the data is
- * 1) A string then create a <p> for it.
- * 2) If it is in an array, then create a <p> per index.
- *
- */
-
-function Description(props) {
-    const description = props.description;
-    // const description = useSelector(state => state.products["round-shieldofthe-bear"].description);
-
-        return (
-        <div className={`product__description ${className}`}>
-                <h3>Description</h3>
-                {description.map((description, index) => {
-                    return (
-                    <p key={index}>{description}</p>
-                    );
-                })}
-            </div>
-        );
+    if(!props.description || props.description === '') {
+        // The description property is either null, undefined,
+        // or an empty string and therefore we should not display
+        // this component at all.
+        return ( null );
     }
 
     return (
-        <div class="item-description">
+        <div className={`product__description ${className}`}>
             <h3>Description</h3>
-            <p>{description}</p>
+            {
+                description.map((description, index) => {
+                    return (<p key={index}>{description}</p>);
+                })
+            }
         </div>
     )
 }
-export default Description;
