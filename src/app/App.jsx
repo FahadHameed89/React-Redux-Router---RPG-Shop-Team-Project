ffbef0f92d1460b44ae5a3fd47d9aa7e4978090c
Danish <danishriz100@gmail.comgit commitexit>
2020-09-23 17:34:22 -0600

    Filter button with hover state done

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -9,6 +9,7 @@ import {
 import ProductList from '../product/ProductList';
 import Product from '../product/Product';
 import SignIn from '../signIn/SignIn';
+import ProductFilter from '../product/ProductFilter';
 
 export default () => {
   return(
@@ -18,14 +19,18 @@ export default () => {
           <li><Link to="/">SignIn</Link></li>
           <li><Link to="/products">Products</Link></li>
           <li><Link to="/products/king-breaker-bow">Kings Breaker Bow</Link></li>
+          <li><Link to="/products/ProductFilter">ProductFilter</Link></li>
+ 
         </ul>
       </nav>
 
       <Switch>
         <Route exact path="/" component={SignIn} />
         <Route exact path="/products" component={ProductList} />
+        <Route path="/products/ProductFilter" component={ProductFilter} />
         <Route path="/products/:id" component={Product} />
-      </Switch>
+      
+       </Switch>
     </Router>
   )
 }
---
+++ src/product/ProductFilter.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import './css/ProductFilter.css'
+
+// FOR SVG https://www.w3schools.com/html/html5_svg.asp
+//https://css-tricks.com/scale-svg/
+
+
+
+function ProductFilter(){
+
+
+  
+
+return(
+    <div className= "product-filter-container">
+        <p>hello</p>
+        
+  <svg className='svg-filter-button' 
+viewBox="0 0 80 80">    
+  <circle className='out-circle'cx="40" cy="40" r="38"  />
+  <circle className='in-circle' cx="40" cy="40" r="34"  />
+
+  <polygon className='up-triangle' points=" 40,13 36,5 44,5 "  />
+
+  <polygon className='up-triangle' points=" 40,67 36,75 44,75 "  />
+
+</svg>
+
+
+
+
+       
+
+  
+
+    
+</div>
+
+);
+
+
+}
+
+export default ProductFilter;
