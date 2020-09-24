6eddc2b1a7aff4ae9c0e910203235d892b9aa597
Irwin-S <irwin.r.singh@gmail.com>
2020-09-24 13:12:50 -0600

    Add filter buttons to product page; added grid to product list; re-used product image component

--- src/product/ProductCard.jsx
+++ src/product/ProductCard.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import ProductImage from './ProductImage';
+
 import './css/product-image.css'
 import './css/product-list.css';
 
-import { Link } from 'react-router-dom';
 
-export default () => {
-    const test = useSelector(state => Object.values(state.products))
+export default (props) => {
+    const product = props.product
     
     return (
-<div className="product-list">
-{
-  test.map((product) => {
-    return (
-      <article key={product.id}>
+      <article className="product-card" key={product.id}>
         <Link to={`/products/${product.id}`}>
-            <div className={`product__image ${product.rarity}`}>
+            {/* <div className={`product__image ${product.rarity}`}>
                 <div className="product__image-rarity">
                     <p>{product.rarity}</p>
                 </div>
                 <img className="product__cardlist" src={`/imgs/products/${product.image}`} alt="" />
-            </div>
+            </div> */}
+
+            <ProductImage path={product.image} rarity={product.rarity} />
             <span>{product.name}</span>
         </Link>
       </article>
-    );
-  })
-}
-</div>
     )
 }
