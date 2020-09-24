1b1b7fe7282744c31c99498107767ffedbf506b3
Irwin-S <irwin.r.singh@gmail.com>
2020-09-24 15:10:11 -0600

    Paired program with Aaron

--- src/product/ProductFilter.jsx
+++ src/product/ProductFilter.jsx
@@ -6,17 +6,23 @@ import "./css/ProductFilter.css";
 //Put image inside SVG https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/SVG_Image_Tag
 
 function ProductFilter(props) {
+  const filter = props.filter     || '';
+  const clicked = props.clicked;
+  const iconName = props.iconName || '';
+
   const path = "/imgs/filter-icons/witch-hat.svg"; //props.path later on
   const path1 = "/imgs/filter-icons/witch-hat-yellow.svg";
-  const filter = "Wizards"; // props.filter here
+  // const filter = "Wizards"; // props.filter here
 
   // on clicking the icon do this action and open the respective list
   const SendParameters = () => {    
     return { role: props.filter, active: true };
   };
 
+  clicked(filter);
+
   return (
-    <div className="product-filter-container" onClick={SendParameters}>
+    <div className="product-filter-container" onClick={() => {clicked(filter)} }>
       <svg
         className="svg-filter-button"
         viewBox="0 0 80 80"
@@ -27,7 +33,7 @@ function ProductFilter(props) {
         <polygon className="up-triangle" points=" 40,13 36,5 44,5 " />
         <polygon className="up-triangle" points=" 40,67 36,75 44,75 " />
 
-        <image href={path} x="15" y="15" height="45px" width="45px" />
+        <image href={`/imgs/filter-icons/${iconName}`} x="15" y="15" height="45px" width="45px" />
       </svg>
 
       <p className="filter-string">{filter}</p>
--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -7,24 +7,32 @@ import ProductCard from './ProductCard';
 
 import './css/product-list.css';
 
-export default () => {
+export default function() {
   const products = useSelector(state => Object.values(state.products));
+  const [filteredProducts, setFilteredProducts] = useState(products);
+  
 
   const saleProduct = products[0];
   const discount = {price: 500, text: "50% off"}
+
+  const filterClicked = function(filter) {
+    console.log(filter + "hi");
+    // const newProducts = this.products.map(x => x.role === "warrior");
+    // this.setFilteredProducts(newProducts);
+  }
 
   return (
     <main className="products container">
       
       <div className="product-filters">
-        <FilterButton filter="warrior"/>
-        <FilterButton filter="mage"/>
-        <FilterButton filter="rogue"/>
+        <FilterButton filter="warrior" clicked={filterClicked} iconName="witch-hat.svg"/>
+        <FilterButton filter="mage"    clicked={filterClicked} iconName="witch-hat.svg"/>
+        <FilterButton filter="rogue"   clicked={filterClicked} iconName="witch-hat.svg"/>
       </div>
 
       <div className="product-list">
         {
-          products.map((product) => {
+          filteredProducts.map((product) => {
             return <ProductCard product={product} />
           })
         }
