372cf4b152b0f0a72efa7d355caa81c828a78dc1
Irwin-S <irwin.r.singh@gmail.com>
2020-09-24 20:51:40 -0600

    Added clan funds component to Header; Added css styling to clan funds component

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import UserProfile from './UserProfile'
 import './css/header.css';
 
 export default () => {
+  const funds = useSelector (state => state.member.funds)
+
   return (
     <header className="main-header container">
+
       <h1>Grimburg's Shop</h1>
 
+      <div className="total-funds">
+          <img src='/imgs/gold.svg' /> 
+          <p>{funds}</p>
+        </div>
       <UserProfile />
     </header>
   );
