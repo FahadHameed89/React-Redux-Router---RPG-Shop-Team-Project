1dc8d783d15ab31b5e07009190a215e12996d5b0
Aaron Barthel <aaron.barthel@live.ca>
2020-09-25 12:53:49 -0600

    Add and implement shopping cart icon

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import UserProfile from './UserProfile'
+import ShoppingCartIcon from '../shoppingCart/ShoppingCartIcon'
+
 import './css/header.css';
 
 export default () => {
@@ -8,11 +10,12 @@ export default () => {
 
   return (
     <header className="main-header container">
+      <ShoppingCartIcon />
 
       <h1>Grimburg's Shop</h1>
 
       <div className="total-credit">
-          <img src='/imgs/gold.svg' /> 
+          <img src='/imgs/gold.svg' />
           <p>{credit}</p>
         </div>
       <UserProfile />
