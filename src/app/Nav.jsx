cdb9678043cab272b24f8877bb0447e6c8180c79
Irwin-S <irwin.r.singh@gmail.com>
2020-09-25 19:42:09 -0600

    Added CSS styling to Nav; includes: hover function

--- src/app/Nav.jsx
+++ src/app/Nav.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './css/nav.css'
 
 export default () => {
 
     return (
     <nav className="shop-nav">
         <Link to="/products">
-            <img src="/imgs/left-arrow.svg" alt="" role="presentation" />
-            Shop
+        <div className="arrow-container">
+            <p><i className="arrow left"></i>Shop</p>
+        </div>
         </Link>
     </nav>
     )
 }
+
+//<img src="/imgs/left-arrow.svg" alt="" role="presentation" />
