ff367478986dbc8cca12ced54cdf317b53931a55
Aaron Barthel <aaron.barthel@live.ca>
2020-09-27 16:54:07 -0600

    Attach avatar image data to the user account icon

--- src/app/UserProfile.jsx
+++ src/app/UserProfile.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import './css/user-profile.css';
+import { useSelector } from 'react-redux';
 
+import './css/user-profile.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default () => {
+  const profile = useSelector(state => state.member);
+
   const [isOpen, setIsOpen] = useState(false);
 
   const signout = () => {
@@ -14,7 +17,7 @@ export default () => {
   return (
     <div className="dropdown">
       <div className="user-account dropdown__button" onClick={() => setIsOpen(true)}>
-        <img src="/imgs/warrior-avatar.svg" alt=""/>
+        <img src={`/imgs/profiles/${profile.profileImg}`} alt="User profile avatar"/>
       </div>
 
       { isOpen &&
